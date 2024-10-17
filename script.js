5d16d0958871b873e43ee3fca908cbb359ae76fc
ritikaphogat01 <145350128+ritikaphogat01@users.noreply.github.com>
2024-10-17 11:47:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+
+
+
+const GuessNumber = document.querySelector(".number");
+const usernumber = document.querySelector(".guess");
+const checkbutn = document.querySelector(".check");
+const outputmessage = document.querySelector(".message");
+const scoreelement = document.querySelector(".score");
+const highscoreelemtent = document.querySelector(".highscore");
+const againbtn = document.querySelector(".again");
+
+let randomnumber = Math.trunc(Math.random() * 20 + 1);
+
+let numberfound = false;
+
+let isPlaying = true;
+
+let score = 20;
+scoreelement.textContent = score;
+let highscore = 0;
+highscoreelemtent.textContent = highscore;
+
+checkbutn.addEventListener("click", function () {
+  let userguessnumber = Number(usernumber.value);
+  console.log(userguessnumber);
+
+  if (!(score === 0)) {
+    if (isPlaying) {
+      if (userguessnumber > randomnumber) {
+        outputmessage.textContent = "Number is Too Larger";
+        usernumber.value = "";
+        score--;
+        scoreelement.textContent = score;
+      } else if (userguessnumber < randomnumber) {
+        outputmessage.textContent = "Number is Too Small";
+        usernumber.value = "";
+        score--;
+        scoreelement.textContent = score;
+      } else if (userguessnumber === randomnumber) {
+        outputmessage.textContent = "🥳🎉 Correct";
+        document.querySelector("body").style.backgroundColor = "green";
+
+        if (score > highscore) {
+          highscore = score;
+          highscoreelemtent.textContent = highscore;
+        }
+        isPlaying = false;
+      } else if (isNaN(userguessnumber)) {
+        outputmessage.textContent = "Invalid Input";
+        usernumber.value = "";
+        score--;
+        scoreelement.textContent = score;
+      }
+    } else {
+      console.log();
+    }
+  } else {
+    outputmessage.textContent = "You loose.";
+  }
+});
+
+console.log(randomnumber);
+
+// afteragain:
+
+againbtn.addEventListener("click", function () {
+  document.querySelector("body").style.backgroundColor = "#222";
+  usernumber.value = "";
+  randomnumber = Math.trunc(Math.random() * 20 + 1);
+
+  score = 20;
+  scoreelement.textContent = score;
+  isPlaying = true;
+  // outputmessage.textContent=message;
+  outputmessage.textContent = "Start guessing...";
+});
